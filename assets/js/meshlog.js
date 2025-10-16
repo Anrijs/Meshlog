31d641fd89f361a0fd1bd36d77f9266a317c6120
Anrijs <anrijs@anrijs.lv>
2025-10-16 17:01:20 +0000

    js: fix sort by name with some emojis

--- assets/js/meshlog.js
+++ assets/js/meshlog.js
@@ -456,7 +456,7 @@ class MeshLogContact extends MeshLogObject {
 
         const removeEmojis = (str) => {
             return str.replace(
-                /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|\uD83E[\uDD00-\uDFFF])/g,
+                /([\u200D\uFE0F]|[\u2600-\u27BF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|\uD83E[\uDD00-\uDFFF])/g,
                 ''
             );
         };
